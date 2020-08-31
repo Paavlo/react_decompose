34de48a93db1ad8239462c76a25063b11403b9b3
unknown <pavlo.com.5@gmail.com>
2020-08-31 15:42:22 +0300

    Updates

---
+++ precache-manifest.786b93c5a78bfece20eada5614af4fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c365f9d497d8a46a6f58d9a85b1348bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "acffcd35b1107d928eaf",
+    "url": "./static/css/main.d652ebea.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acffcd35b1107d928eaf",
+    "url": "./static/js/main.310ab3c8.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
